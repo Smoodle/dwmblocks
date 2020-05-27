d00eab7b1088cad951c6ba57f62f42d4f60c2157
Smoodle <migwwe@gmail.com>
2020-05-27 10:54:42 +0100

    first commit

---
+++ blocks.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "~/.scripts/bar/music", 2, 0},
+	{"", "~/.scripts/bar/updates", 600, 0},
+	{"", "~/.scripts/bar/disk", 300, 0},
+	{"", "~/.scripts/bar/cpu", 10, 0},
+	{"", "~/.scripts/bar/ram", 10, 0},
+	{"", "~/.scripts/bar/date", 60, 0},
+	{"", "~/.scripts/bar/time", 5, 20},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = ' ';
