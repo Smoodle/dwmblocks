8546ce533a98941786a87c1a3ba594bd9a97e94e
Smoodle <migwwe@gmail.com>
2020-05-27 11:10:00 +0100

    Added new scripts to blocks folder

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/.scripts/bar/music", 2, 0},
-	{"", "~/.scripts/bar/updates", 600, 0},
-	{"", "~/.scripts/bar/disk", 300, 0},
-	{"", "~/.scripts/bar/cpu", 10, 0},
-	{"", "~/.scripts/bar/ram", 10, 0},
-	{"", "~/.scripts/bar/date", 60, 0},
-	{"", "~/.scripts/bar/time", 5, 20},
+	{"", "~/.scripts/blocks/music", 2, 0},
+	{"", "~/.scripts/blocks/updates", 600, 0},
+	{"", "~/.scripts/blocks/disk", 300, 0},
+	{"", "~/.scripts/blocks/cpu", 10, 0},
+	{"", "~/.scripts/blocks/ram", 10, 0},
+	{"", "~/.scripts/blocks/date", 60, 0},
+	{"", "~/.scripts/blocks/time", 5, 20},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
